260b597381532b068aa27149d7e28dbac2c35216
Luke Bradley <bradleyla@g.cofc.edu>
2017-02-07 15:16:45 -0500

    Initial commit

---
+++ app/src/main/java/com/introtoandroid/inclass_activity_01_1_bradley/InclassActivity01Bradley.java
@@ -0,0 +1,13 @@
+package com.introtoandroid.inclass_activity_01_1_bradley;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InclassActivity01Bradley extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inclass_activity01_bradley);
+    }
+}
